6c5053d5dc613b131c16b16846da352ca1aa942b
vesloguzov <vesloguzov@gmail.com>
2018-09-13 12:57:17 +0500

    init#33

--- dsp/static/js/src/dsp.js
+++ dsp/static/js/src/dsp.js
@@ -1,24 +1,42 @@
 /* Javascript for DSPXBlock. */
+
+function parseTextSignal(signal_string) {
+    var signal_array = signal_string.replace('[', '').replace(']', '').replace('(', '').replace(')', '').split(/[ ,]+/);
+    var cleaned_array = signal_array.filter(function (item) {
+        return item != ""
+    });
+    signal_valid = cleaned_array.every((item) => !isNaN(parseFloat(item)));
+    if (signal_valid) {
+        signal = cleaned_array.map(function (item) {
+            return parseFloat(item)
+        });
+        return {"signal": signal, "signal_valid": signal_valid};
+    }
+    else {
+        return {"signal": [], "signal_valid": signal_valid};
+    }
+}
+
 /*
 function DSPXBlock(runtime, element) {
 
-    function updateCount(result) {
-        $('.count', element).text(result.count);
-    }
+function updateCount(result) {
+$('.count', element).text(result.count);
+}
 
-    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+var handlerUrl = runtime.handlerUrl(element, 'increment_count');
 
-    $('p', element).click(function(eventObject) {
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
-        });
-    });
+$('p', element).click(function(eventObject) {
+$.ajax({
+    type: "POST",
+    url: handlerUrl,
+    data: JSON.stringify({"hello": "world"}),
+    success: updateCount
+});
+});
 
-    $(function ($) {
+$(function ($) {
 
-    });
+});
 }
 */
