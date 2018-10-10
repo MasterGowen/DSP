585c6772557d489763f3a13240c1130c90ccd170
vesloguzov <vesloguzov@gmail.com>
2018-10-10 12:57:24 +0500

    init#151

--- dsp/static/lab_1/lab_1.js
+++ dsp/static/lab_1/lab_1.js
@@ -15,6 +15,25 @@ function DSPXBlock(runtime, element, data) {
                 $("#graphic_1", element).html(result["graphics"][0]["html"]);
                 $("#graphic_2", element).html(result["graphics"][1]["html"]);
             },
+            error: function (jqXHR, exception) {
+                var msg = '';
+                if (jqXHR.status === 0) {
+                    msg = 'Not connect.\n Verify Network.';
+                } else if (jqXHR.status == 404) {
+                    msg = 'Requested page not found. [404]';
+                } else if (jqXHR.status == 500) {
+                    msg = 'Internal Server Error [500].';
+                } else if (exception === 'parsererror') {
+                    msg = 'Requested JSON parse failed.';
+                } else if (exception === 'timeout') {
+                    msg = 'Time out error.';
+                } else if (exception === 'abort') {
+                    msg = 'Ajax request aborted.';
+                } else {
+                    msg = 'Uncaught Error.\n' + jqXHR.responseText;
+                }
+                console.log(msg);
+            },
             contentType: 'application/json; charset=utf-8'
         });
     }
