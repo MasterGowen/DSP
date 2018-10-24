7f0a4231b4266da313ad2ec0c8ae5894c9161e15
vesloguzov <vesloguzov@gmail.com>
2018-10-24 16:13:29 +0500

    init#297

--- dsp/lab_4.py
+++ dsp/lab_4.py
@@ -88,8 +88,6 @@ def lab_4_check_answer(student_data, source_data):
     student_b = float(student_data["student_b"])
     student_d = student_data["student_signal"]
     student_a = student_data["student_filter"]
-
-
     student_F = float(student_data["student_F"])
     student_Dp = float(student_data["student_Dp"])
     student_filter_stable = student_data["student_filter_stable"]
@@ -97,16 +95,25 @@ def lab_4_check_answer(student_data, source_data):
     a_et = get_correct_filter(source_data)
     b_et = [1]
     d_et = get_correct_signal(source_data)
+    N0 = len(d_et)
 
     z_et = signal.lfilter(b_et, a_et, d_et)
     fz_et = np.abs(np.fft.fft(z_et))
 
     f707 = [1 if x > 0.707*max(fz_et) else 0 for x in fz_et]
-    Fp = N0/2 - (np.where(np.flip(f707)[int(N0/2):] == 1)[0][0]+1) + 1
+    F_et = N0/2 - (np.where(np.flip(f707)[int(N0/2):] == 1)[0][0]+1) + 1
     mz = [1 if x > 0.05*max(abs(z_et)) else 0 for x in abs(z_et)]
-    Dp = N0 - (np.where(np.flip(mz) == 1)[0][0]+1)
+    Dp_et = N0 - (np.where(np.flip(mz) == 1)[0][0]+1)
 
+    if abs(float(source_data["a1"])) > 1:
+        is_stable = "stable"
+    else:
+        is_stable = "unstable"
 
+    max_score = 6
+    score = 0
+    result = dict()
+    result["correctness"] = dict()
     if arrays_is_equal(d_et, student_d):
         result["correctness"]["signal_correctness"] = True
         score += 1
@@ -114,12 +121,45 @@ def lab_4_check_answer(student_data, source_data):
         result["correctness"]["signal_correctness"] = False
     result["correctness"]["signal_correct"] = d_et.tolist()
 
+    if numbers_is_equal(b_et[0], student_b, tol=0.01):
+        result["correctness"]["b_correctness"] = True
+        score += 1
+    else:
+        result["correctness"]["b_correctness"] = False
+    result["correctness"]["b_correct"] = b_et
+
     if arrays_is_equal(a_et, student_a):
         result["correctness"]["filter_correctness"] = True
         score += 1
     else:
         result["correctness"]["filter_correctness"] = False
-    result["correctness"]["filter_correct"] = b_et.tolist()
+    result["correctness"]["filter_correct"] = a_et.tolist()
+
+    if numbers_is_equal(F_et, student_F, tol=0.1):
+        result["correctness"]["F_correctness"] = True
+        score += 1
+    else:
+        result["correctness"]["F_correctness"] = False
+    result["correctness"]["F_correct"] = F_et
+
+    if numbers_is_equal(Dp_et, student_Dp, tol=0.1):
+        result["correctness"]["Dp_correctness"] = True
+        score += 1
+    else:
+        result["correctness"]["Dp_correctness"] = False
+    result["correctness"]["Dp_correct"] = Dp_et
+
+    if is_stable == student_filter_stable:
+        result["correctness"]["filter_stable_correctness"] = True
+        score += 1
+    else:
+        result["correctness"]["filter_stable_correctness"] = False
+    result["correctness"]["filter_stable_correct"] = is_stable
+
+    result["score"] = float(score) / float(max_score)
+
+    return result
+
 
 def lab_4_get_graphics(student_data, source_data):
     graphics = []
