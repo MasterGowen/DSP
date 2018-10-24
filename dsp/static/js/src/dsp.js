4a9080185db49544b83182b5e6ebd299d890a293
vesloguzov <vesloguzov@gmail.com>
2018-10-24 16:50:33 +0500

    init#308

--- dsp/static/js/src/dsp.js
+++ dsp/static/js/src/dsp.js
@@ -44,6 +44,7 @@ function process_array_input(input) {
 function highlight_correctness(state) {
     Object.keys(state).forEach(function (item) {
         console.log($("#input_student_" + item.split("_")[0]));
+        console.log(state[item], item);
 
         if (state[item] == true) {
             $("#input_student_" + item.split("_")[0]).removeClass("dsp-incorrect-input");
