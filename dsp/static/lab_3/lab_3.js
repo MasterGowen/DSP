f207920263e3063f6f1addd3f709f4dcb92ed943
vesloguzov <vesloguzov@gmail.com>
2018-10-25 16:26:47 +0500

    init#340

--- dsp/static/lab_3/lab_3.js
+++ dsp/static/lab_3/lab_3.js
@@ -115,7 +115,7 @@ function DSPXBlock(runtime, element, data) {
     // }
 
     $(function ($) {
-
+        console.log(data);
         if (data.student_state.answer) {
             build_lab_state(data["student_state"]);
             $("textarea.array-input", element).each(function (i) {
