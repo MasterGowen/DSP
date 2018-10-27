3273a3b841657034852896bc9a0f8ec2d78d5e86
vesloguzov <vesloguzov@gmail.com>
2018-10-27 18:10:53 +0500

    init#400

--- dsp/lab_3.py
+++ dsp/lab_3.py
@@ -27,7 +27,7 @@ def lab_3_get_source_data():
     ]
     signal_type = random.choice(signal_types)  # signal_types[0]  #
 
-    s = [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8]
+    s = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8]
     # s = [0.0, 0.2, 0.4, 0.6, 0.8]
 
     context = dict()
