8bf308cbd98e4d7ea2e5444046188de1f86f76c4
vesloguzov <vesloguzov@gmail.com>
2018-11-03 16:08:03 +0500

    init#517

--- dsp/lab_3.py
+++ dsp/lab_3.py
@@ -137,9 +137,9 @@ def lab_3_get_graphic_2(correct_answer, student_data, source_data, reload="True"
             s2 = signal.lfilter(b, 1, y2)
             w = (np.array(s2) > np.array(pp)).astype(int)
             log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            log.info(math.floor(N0-K/2)-1)
-            log.info(math.floor(N0+K/2)+3)
-            for x in np.arange(math.floor(N0-K/2), math.floor(N0+K/2)+3):
+            log.info(math.floor(N0 - float(K)/2)-1)
+            log.info(math.floor(N0 + float(K)/2)+3)
+            for x in np.arange(math.floor(N0-float(K)/2), math.floor(N0+float(K)/2)+3):
                 w[x-1] = 0
             q = q + np.double(sum(w) > 0)
         if Ku_i == 10:
