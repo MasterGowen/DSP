25d6e0622dbf862bf2f9b5c8797de2a29aa08152
vesloguzov <vesloguzov@gmail.com>
2018-11-06 18:29:27 +0500

    init#533

--- dsp/lab_5.py
+++ dsp/lab_5.py
@@ -33,6 +33,21 @@ def lab_5_get_source_data():
         "name": "lchm_signal",
         "title": "Сигнал с ЛЧМ с параметрами \(T = {}\),\(f_e = {}\),\(f_s = {}\), амплитуда = 1".format(T, fe, fs)
     }
+
+    K_1 = 2
+    K_2 = 5
+
+    s2 = signal.lfilter(np.array([1, 1, 1]) / 3, [1, -0.85], np.random.randn(250))
+    s31 = s2[0::2]
+    s32 = s2[0::5]
+    s33 = signal.decimate(s2, 2, ftype='fir')
+    s34 = signal.decimate(s2, 5, ftype='fir')
+
+    s41 = signal.resample_poly(s31, 2, 1)
+    s42 = signal.resample_poly(s32, 5, 1)
+    s43 = signal.resample_poly(s33, 2, 1)
+    s44 = signal.resample_poly(s34, 5, 1)
+
     context = dict()
     context["fd"] = fd
     context["f0"] = f0
@@ -41,6 +56,16 @@ def lab_5_get_source_data():
     context["fs"] = fs
     context["K1"] = K1
     context["K2"] = K2
+    context["s2"] = s2
+
+    context["K_1"] = K_1
+    context["K_2"] = K_2
+
+    context["s41"] = s41
+    context["s42"] = s42
+    context["s43"] = s43
+    context["s44"] = s44
+
     context["signal_type_1"] = signal_type_1
     context["signal_type_2"] = signal_type_2
     return context
