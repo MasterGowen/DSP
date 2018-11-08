9d9565e47fd39cc8194f0b20a7f67d5c0949e0b7
vesloguzov <vesloguzov@gmail.com>
2018-11-08 14:19:58 +0500

    init#550

--- dsp/static/lab_5/lab_5.js
+++ dsp/static/lab_5/lab_5.js
@@ -75,86 +75,6 @@ function DSPXBlock(runtime, element, data) {
         });
     }
 
-    // function build_graphic_2(reload, there_is_signal="") {
-    //     show_graphic_load($('#graphic_2', element));
-    //     disable($('#calculate_graphic_2_there_is_signal', element));
-    //     disable($('#calculate_graphic_2_there_is_no_signal', element));
-    //     disable($('#calculate_graphic_2', element));
-    //     params = [];
-    //     params_str = "";
-    //     if (reload) {
-    //         params.push("reload=true");
-    //     }
-    //     if (there_is_signal !== ""){
-    //         params.push("is_signal="+there_is_signal)
-    //     }
-    //     if (params.length > 0){
-    //         params_str = "?" + params.join("&")
-    //     }
-    //     $.ajax({
-    //         type: "POST",
-    //         url: get_graphic_2 + params_str,
-    //         data: JSON.stringify(generateAnswer()),
-    //         success: function (result) {
-    //              $(".graphic-2-not-first-build", element).css("display", "block");
-    //
-    //              $("#graphic_2", element).html(result["graphic"]["html"]);
-    //              $("#current_Ku_i", element).html(result["student_state"]["state"]["Ku_i"]);
-    //              $("#current_Ku_j", element).html(result["student_state"]["state"]["Ku_j"]);
-    //
-    //              $("#there-is-no-signal-count", element).html(result["student_state"]["state"]["there_is_no_signal_count"]);
-    //              $("#there-is-signal-count", element).html(result["student_state"]["state"]["there_is_signal_count"]);
-    //
-    //              result["student_state"]["state"]["there_is_signal_states"].forEach(function(state_value, idx) {
-    //                 if (state_value.there_is_signal_count !== undefined ) {
-    //                     $("#input_student_s .label-signal-count", element)[idx].innerHTML = state_value.there_is_signal_count;
-    //                     $("#input_student_s .label-no-signal-count", element)[idx].innerHTML = state_value.there_is_no_signal_count;
-    //                 }
-    //                 else {
-    //                     $("#input_student_s .label-signal-count", element)[idx].innerHTML = 0;
-    //                     $("#input_student_s .label-no-signal-count", element)[idx].innerHTML = 0;
-    //                 }
-    //
-    //             });
-    //
-    //              if (result["student_state"]["state"]["Ku_done"]){
-    //                  $("#graphic-2-controls", element).css("display", "none");
-    //              }
-    //              else{
-    //                  $("#graphic-2-controls", element).css("display", "block");
-    //              }
-    //             enable($('#calculate_graphic_2_there_is_signal', element));
-    //             enable($('#calculate_graphic_2_there_is_no_signal', element));
-    //             enable($('#calculate_graphic_2', element));
-    //         },
-    //         error: function (jqXHR, exception) {
-    //             show_graphic_error($('#graphic_2', element));
-    //             log_ajax_error(jqXHR, exception);
-    //             enable($('#calculate_graphic_2_there_is_signal', element));
-    //             enable($('#calculate_graphic_2_there_is_no_signal', element));
-    //             enable($('#calculate_graphic_2', element));
-    //         },
-    //         contentType: 'application/json; charset=utf-8'
-    //     });
-    // }
-    //
-    // function build_graphic_3() {
-    //     show_graphic_load($('#graphic_3', element));
-    //     $.ajax({
-    //         type: "POST",
-    //         url: get_graphic_3,
-    //         data: JSON.stringify(generateAnswer()),
-    //         success: function (result) {
-    //             $("#graphic_3", element).html(result["graphic"]["html"]);
-    //         },
-    //         error: function (jqXHR, exception) {
-    //             show_graphic_error($('#graphic_3', element));
-    //             log_ajax_error(jqXHR, exception);
-    //         },
-    //         contentType: 'application/json; charset=utf-8'
-    //     });
-    // }
-
     // $('#check_answer', element).click(function (event) {
     //     console.info("Начали проверку");
     //     $.ajax({
@@ -179,21 +99,6 @@ function DSPXBlock(runtime, element, data) {
     $('#calculate_graphic_2', element).click(function (event) {
         build_graphic_2();
     });
-
-    // $('#calculate_graphic_2', element).click(function (event) {
-    //     build_graphic_2(true);
-    // });
-    // $('#calculate_graphic_2_there_is_signal', element).click(function (event) {
-    //     build_graphic_2(false, "there_is_signal");
-    // });
-    // $('#calculate_graphic_2_there_is_no_signal', element).click(function (event) {
-    //     build_graphic_2(false, "there_is_no_signal");
-    // });
-    // $('#reset_task', element).click(function (event) {
-    //     reset_task();
-    // });
-
-
 
 
     function generateAnswer() {
@@ -231,26 +136,39 @@ function DSPXBlock(runtime, element, data) {
         build_graphic_2();
     }
 
-    // function buttons_disable() {
-    //     var student_data = generateAnswer();
-    //     if (student_data.student_filter.length > 0 && student_data.student_signal.length > 0) {
-    //         enable($("#calculate_graphic_1", element));
-    //         enable($("#calculate_graphic_2", element));
-    //         enable($("#reset_task", element));
-    //         if (parseFloat(student_data.student_B) && student_data.student_s.every(elem => elem.toString().replace(/\s/g, "") != "")) {
-    //             enable($("#check_answer", element));
-    //         }
-    //         else {
-    //             disable( $("#check_answer", element));
-    //         }
-    //     }
-    //     else {
-    //         disable($("#calculate_graphic_1", element));
-    //         disable($("#calculate_graphic_2", element));
-    //         disable($("#reset_task", element));
-    //         disable($("#check_answer", element));
-    //     }
-    // }
+    function buttons_disable() {
+        var student_data = generateAnswer();
+        if (student_data.student_s.length > 0 && student_data.student_s1.length > 0) {
+            enable($("#calculate_graphic_1", element));
+        }
+        else {
+            disable($("#calculate_graphic_1", element));
+        }
+        if (student_data.student_sl.length > 0 && student_data.student_slc.length > 0) {
+            enable($("#calculate_graphic_2", element));
+        }
+        else{
+            disable($("#calculate_graphic_2", element));
+        }
+        if (student_data.student_s.length > 0 && student_data.student_s1.length > 0 && student_data.student_sl.length > 0 && student_data.student_slc.length > 0){
+            if (parseFloat(student_data.student_fn) && parseFloat(student_data.student_Np){
+                if (parseFloat(student_data.student_K1) && parseFloat(student_data.student_K2) && parseFloat(student_data.student_K3) && parseFloat(student_data.student_K4)){
+                    enable($("#check_answer", element));
+                }
+                else{
+                    disable($("#check_answer", element));
+                }
+            }
+            else{
+                disable($("#check_answer", element));
+             }
+        }
+        else{
+            disable($("#check_answer", element));
+        }
+
+
+    }
 
     $(function ($) {
         console.log(data);
@@ -263,29 +181,21 @@ function DSPXBlock(runtime, element, data) {
             //     highlight_correctness(data["student_state"]["correctness"]);
             // }
         }
-        // buttons_disable();
+        buttons_disable();
 
-        // $(element).on('input', ".answer-input", function () {
-            // buttons_disable();
+        $(element).on('input', ".answer-input", function () {
+            buttons_disable();
             // if (highlight_correct) {
             //     $(this).removeClass("dsp-incorrect-input");
             //     $(this).removeClass("dsp-correct-input");
             // }
-        // });
-
+        });
 
         $("textarea.array-input", element).each(function (i) {
             $(this).change(function () {
                 process_array_input(this);
             });
         });
-
-        // $("input.s-input", element).each(function (i) {
-        //     $(this).change(function () {
-        //         build_graphic_3();
-        //     });
-        // })
-
 
     });
 
