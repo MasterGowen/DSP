7f222d8792b1df58e7c384165dccb871a3dc5534
vesloguzov <vesloguzov@gmail.com>
2018-11-09 16:27:27 +0500

    init#580

--- dsp/lab_5.py
+++ dsp/lab_5.py
@@ -118,8 +118,6 @@ def lab_5_check_answer(student_data, source_data):
     s_et, s1_et = get_correct_signals_1(source_data)
     log.info(abs(np.fft.fft(s1_et)))
     m1_et, mi_et = abs(np.fft.fft(s1_et)).max(0), np.argmax(abs(np.fft.fft(s1_et))) + 1
-    log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
-    log.info(m1_et)
     log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     log.info(mi_et)
     fn_et = fd * mi_et / len(s1_et)
