b3376fdde5c7a763e9e79ca1de4215dc6196de25
vesloguzov <vesloguzov@gmail.com>
2018-11-10 18:28:40 +0500

    init#621

--- dsp/dsp.py
+++ dsp/dsp.py
@@ -188,7 +188,7 @@ class DSPXBlock(XBlock):
         #     return Response({'exception': "ValueError"}, 500)
         except Exception as e:
             log.info(e)
-            return Response(e, status=500)
+            return Response(json.dumps({"exception": e}), status=500)
 
 
     @XBlock.json_handler
