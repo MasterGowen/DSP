8a46c36073fed571fe1f820ae01d21ba43de8dd3
vesloguzov <vesloguzov@gmail.com>
2018-11-24 13:55:14 +0500

    init#777

--- dsp/dsp.py
+++ dsp/dsp.py
@@ -338,11 +338,11 @@ class DSPXBlock(XBlock):
 
     @XBlock.handler
     def lab_7_get_graphic_1(self, data, suffix=''):
-        try:
-            graphics = lab_7_get_graphic_1(self.lab_source_data, self.correct_answer)
-            return Response(json_body={"graphics": graphics})
-        except:
-            return Response('Error!', 500)
+        # try:
+        graphics = lab_7_get_graphic_1(self.lab_source_data, self.correct_answer)
+        return Response(json_body={"graphics": graphics})
+        # except:
+        #     return Response('Error!', 500)
 
     @XBlock.json_handler
     def lab_7_get_graphic_2(self, data, suffix=''):
