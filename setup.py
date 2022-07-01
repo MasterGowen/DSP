fc96a1f60180fa8ed6aa81ae19fcce028adaa6c0
MasterGowen <mastergowen@gmail.com>
2022-07-01 14:31:30 +0500

    upd version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def package_data(pkg, roots):
 
 setup(
     name='dsp-xblock',
-    version='0.1',
+    version='0.1.1',
     description='digital signal processing XBlock',  # TODO: write a better description.
     license='UNKNOWN',  # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
     packages=[
